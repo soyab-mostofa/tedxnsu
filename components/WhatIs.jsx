f159bf0e0ee3673cd1a9ae1fe137892b4f8bcc13
soyab-mostofa <54860282+soyab-mostofa@users.noreply.github.com>
2022-09-28 18:32:54 +0600

    herosection

--- components/WhatIs.jsx
+++ components/WhatIs.jsx
@@ -5,17 +5,17 @@ import { Heading1 } from './Styles';
 const WhatIs = () => {
   return (
     <div className="text-sm px-2 container max-w-7xl mx-auto py-4">
-      <div className="bg-white text-neutral-900 rounded-2xl mx-10 p-4">
-        <div>
+      <div className="bg-white md:flex md:justify-around md:items-center text-neutral-900 rounded-2xl mx-10 p-4">
+        <div className="">
           <Heading1>
             What is
-            <span className=" text-[#FF2B06]">
+            <span className=" text-[#FF2B06] inline-block mx-2">
               TED<span className="text-sm text-[#FF2B06]">x</span>
             </span>
-            NorthSouthUniversity?
+            North South University?
           </Heading1>
 
-          <div className="space-y-4">
+          <div className="space-y-4 md:max-w-sm px-4">
             <p>
               TEDxNorthSouthUniversity is an independently organized TED
               university event where the speakers will share their ideas of
@@ -36,7 +36,7 @@ const WhatIs = () => {
             </p>
           </div>
         </div>
-        <div className="relative h-72 w-96 mt-4">
+        <div className="relative h-72 w-full mt-4 basis-1/2">
           <Image
             src="/nsu-front.png"
             layout="fill"
